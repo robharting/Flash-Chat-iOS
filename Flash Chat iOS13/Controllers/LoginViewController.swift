5140ba38fba4f114024550a1d32e40c85809429e
Rob Harting <robharting@hotmail.com>
2024-09-13 13:12:14 +0200

    added login logic

--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
-
-    @IBAction func loginPressed(_ sender: UIButton) {
-    }
     
+    @IBAction func loginPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                
+                if let e = error {
+                    // in case of prod do a pop up or show in a label via first mapping all the codes in a switch
+                    print(e.localizedDescription)
+                } else {
+                    // Login and Navigate to the chat view controller
+                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+                
+            }
+        }
+    }
 }
