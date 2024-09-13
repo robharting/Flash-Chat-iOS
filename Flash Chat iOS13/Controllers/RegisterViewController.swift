1a16f2fff19943bdc64a9a49d792e823bba25c5e
Rob Harting <robharting@hotmail.com>
2024-09-13 12:45:58 +0200

    added login registring

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
@@ -14,6 +15,21 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    // in case of prod do a pop up or show in a label
+                    print(e.localizedDescription)
+                } else {
+                    // Navigate to the chat view controller
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+                
+            }
+        }
+        
+        
     }
     
 }
