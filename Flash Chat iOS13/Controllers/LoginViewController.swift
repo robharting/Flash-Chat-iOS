9918ea2f3df0e368c00ff4f8b82dcc55ac57fd58
Rob Harting <robharting@hotmail.com>
2024-09-13 17:13:14 +0200

    refactor seperate stings to constant file via static - Type Properties

--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -25,7 +25,7 @@ class LoginViewController: UIViewController {
                     print(e.localizedDescription)
                 } else {
                     // Login and Navigate to the chat view controller
-                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                    self.performSegue(withIdentifier: K.loginSegue, sender: self)
                 }
                 
             }
