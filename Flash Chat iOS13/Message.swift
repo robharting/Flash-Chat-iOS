b30ecf41a02cb5cdb10fe4f7ab761171ea4c6fb7
Rob Harting <robharting@hotmail.com>
2024-09-16 08:00:55 +0200

    first steps to configure UITTableView and cell:

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Harting, R.P.G. (Rob) on 16/09/2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String // 1@2.com
+    let body: String
+}
